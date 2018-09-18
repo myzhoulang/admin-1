c7dbd49a9f014bcd9f5edcd189f5df8c87d86e4f
周浪 <lang.zhou@fulintechfin.com>
2018-09-18 19:04:02 +0800

    :bookmark: GitHub homepage静态资源处理

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,14 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less-modules');
 const rewireMobX = require('react-app-rewire-mobx');
+const rewireWebpackOutput = require('react-app-rewire-output');
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }, {libraryName: 'bizcharts', libraryDirectory: 'es6'}], config);
+
+  config = rewireWebpackOutput(config, env, {
+    publicPath: '/admin-1'
+  })
 
   config = rewireLess(config, env);
   config = rewireLess.withLoaderOptions({
